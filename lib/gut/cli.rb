8f632bc1500a39577e0ae314e5a66a61d46a1d48
Andrew Hoglund <ahoglund@tunecore.com>
2016-07-15 18:00:28 -0400

    push and pull

--- lib/gut/cli.rb
+++ lib/gut/cli.rb
@@ -2,7 +2,14 @@ require "gut"
 require "thor"
 
 class Gut::CLI < Thor
-  def start(process=nil)
-    puts "Started"
+
+  desc "push", "Add something to your gut"
+  def push(food)
+    puts "Added #{food} to your gut"
+  end
+
+  desc "pull", "Find something for your gut"
+  def pull(food)
+    puts "Finding #{food} for your gut..."
   end
 end
